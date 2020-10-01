5a7270b9c6f9c2c97a345597f3833ee23855e0d7
ShubhamRwt <shubhamrwt02@gmail.com>
2020-10-01 18:34:09 +0530

    lec

---
+++ src/com/company/Lec20/MyException.java
@@ -0,0 +1,8 @@
+package com.company.Lec20;
+
+public class MyException extends Exception {
+
+    public MyException(String s) {
+        super(s);
+    }
+}
---
+++ src/com/company/Lec20/Queue.java
@@ -0,0 +1,49 @@
+package com.company.Lec20;
+
+public class Queue {
+
+    private int[] data;
+    private int rear;
+
+    public Queue(){
+
+        this.data = new int[10];
+        rear = -1;
+
+    }
+
+    private boolean isFull(){
+        return rear == data.length-1;
+    }
+
+    public void enqueue(int element){
+
+        if(isFull()){
+            return;
+        }
+
+        data[++rear] = element;
+    }
+
+    public boolean isEmpty(){
+        return rear == -1;
+    }
+
+    public int deque(){
+// 0(n)
+        if(isEmpty()){
+            return -1;
+        }
+
+        int temp =data[0];
+
+        for (int i = 1; i <=rear ; i++) {
+
+            data[i-1]=data[i];
+        }
+
+        rear--;
+        return temp;
+    }
+
+}
